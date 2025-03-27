380a36c036bca32ff5fee789a626c468babdcc20
DonchenkoIN <ihordon4encko@yandex.ru>
2025-03-27 23:26:10 +0300

    first commit

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace DonchenkoIn\TestPackage\Tests;
+use Orchestra\Testbench\TestCase;
+class ExampleTest extends TestCase
+{
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+
+}
